ddd69627ca51b23423b0170bfd15b386386ef7b9
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-20 20:08:05 -0600

    variation of googlemap

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -15,10 +15,21 @@ class  GoogleMap extends React.Component {
   render() {
     return(
       <div ref='map' />
-
-
     )
   }
 }
 
 export default GoogleMap;
+
+//
+// or
+// import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+// export default (props) => {
+//   return(
+//     <GoogleMapLoader
+//       containerElement={ <div style={{height: '100%'}} /> }
+//       googleMapElement={
+//         <GoogleMap defaultZoom={12} defaultCenter={{ lat: props.lat, lng: props.lon}} />
+//       }
+//   )
+// }
